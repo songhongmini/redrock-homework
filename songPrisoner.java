db21d3bef8ec9336d92db8825bd7f1b24ef3b39f
songhongmini <1329723774@qq.com>
2016-10-28 13:24:14 +0800

    Rejected commit: Default

--- songPrisoner.java
+++ songPrisoner.java
@@ -1,28 +1,27 @@
 package com.jude.prisoner;
-import com.jude.Prisoner;
+
 import com.jude.Manager;
+import com.jude.Prisoner;
 
 /**
- * Created by Administrator on 2016/10/25.
+ * Created by Administrator on 2016/10/28.
  */
-public class songPrisoner implements Prisoner {
-    int totalPerson;
+public class songPrisoner  implements Prisoner {
     int totalCount;
-
+    int totalPerson;
     @Override
     public String getName() {
-        return "宋 宏2016212017";
+        return "宋宏2016212017";
     }
-
     @Override
     public void begin(Manager manager, int totalPerson, int totalCount) {
         this.totalCount = totalCount;
         this.totalPerson = totalPerson;
-
     }
 
     @Override
     public int take(int index, int last) {
+        //经过我宋宏缜密的计算，我算出我拿这个数最能活
         return ((totalCount-last)/(index+9) + totalCount/totalPerson)/2;
     }
 
@@ -30,4 +29,8 @@ public class songPrisoner implements Prisoner {
     public void result(boolean survived) {
 
     }
+
+
+
+
 }
